9409cc1ff353dc830cdeb00e13efa8303c0c97bd
panapang <panapang@gmail.com>
2021-10-06 14:39:50 +0700

    clean up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
-import User from "./components/user.component";
-import UserList from "./components/user-list.component";
 import HouseTimeline from './components/houseTimeline.component';
-import AddUser from './components/add-user.component';
 
 class App extends Component {
   render() {
@@ -20,25 +17,12 @@ class App extends Component {
                 House Timeline
               </Link>
             </li>
-            <li className="nav-item">
-              <Link to={"/user"} className="nav-link">
-                User
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/area"} className="nav-link">
-                Area
-              </Link>
-            </li>
           </div>
         </nav>
 
         <div className="container mt-3">
           <Switch>
             <Route exact path={["/", "/houseTimeline"]} component={HouseTimeline} />
-            <Route exact path="/adduser" component={AddUser} />
-            <Route exact path="/user" component={UserList} />
-            <Route path="/user/:id" component={User} />
           </Switch>
         </div>
       </div>
